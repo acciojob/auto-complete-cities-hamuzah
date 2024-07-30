8fa2009b9d9abf3232f53b2d21cccf20f3d186ba
HAMUZA <kiggunduhamuza16@gmail.com>
2024-07-30 04:51:51 -0700

    editt

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,65 @@
 
-import React from "react";
+import React,{useEffect, useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const suggestions = [
+    "Apple",
+    "Banana",
+    "Cherry",
+    "Dragon Fruit",
+    "Elderberry",
+    "Fig",
+    "Grape",
+    "Honeydew",
+    "Jackfruit",
+    "Kiwi",
+    "Lemon",
+    "Mango",
+    "Nectarine",
+    "Orange",
+    "Papaya",
+    "Quince",
+    "Raspberry",
+    "Strawberry",
+    "Tangerine",
+    "Umbrella",
+    "Vinegar",
+    "Watermelon",
+    "Xanadu",
+    "Yogurt",
+    "Zucchini"
+   ]
+
+
+  const[query, setQuery] = useState('')
+  const[filteredsuggestions, setfilteredSuggestions] = useState('')
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setfilteredSuggestions(suggestions.filter((suggestion)=>
+            suggestion.toLowerCase().includes(query.toLowerCase()))
+    )
+    },200)
+  },[query,suggestions])
+
+   
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+  }
+
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Search Item</h1>
+        <input type="text" value={query} onChange={handleChange} />
+        <ul>
+        {filteredsuggestions.map((suggestion) => (
+          <li key={suggestion}>{suggestion}</li>
+        ))}
+      </ul>
     </div>
   )
 }
